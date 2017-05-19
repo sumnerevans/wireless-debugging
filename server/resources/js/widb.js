02b4cdead23b7669dfd79abb2ec11a040c80105b
Sumner Evans <sumner.evans98@gmail.com>
2017-05-19 17:13:24 -0600

    WD-6: fixed a few mor style issues

--- server/resources/js/widb.js
+++ server/resources/js/widb.js
@@ -3,12 +3,14 @@
  */
 
 $(document).ready(() => {
-    $('.log-panel-body').html('Hello world');
-    let ws = new WebSocket('ws://localhost:8080/ws');
-    ws.onopen = () => {
-      console.log('gotHere');
-    }
-    ws.onmessage = () => {
-        console.log('on Message');
-    }
+  $('.log-panel-body').html('Hello world');
+
+  let ws = new WebSocket('ws://localhost:8080/ws');
+  ws.onopen = () => {
+    console.log('gotHere');
+  };
+
+  ws.onmessage = () => {
+    console.log('on Message');
+  };
 });
