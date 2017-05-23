a481ea94610e3a97c4fea8718efae4110b967784
Sumner Evans <sumner.evans98@gmail.com>
2017-05-23 13:58:32 -0600

    WD-17: began implementing the parsing functionality

---
+++ server/parsing_lib/log_parser.py
@@ -0,0 +1,57 @@
+"""
+LogParser class
+"""
+
+import json
+import re
+from datetime import datetime
+
+
+class LogParser(object):
+
+    @staticmethod
+    def parse(message):
+        logEntries = []
+        current_log = None
+        for line in message.rawLogData.splitlines():
+            if re.search('\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}', line) is not None:
+                if current_log is not None:
+                    logEntries.append(LogParser.parse_raw_log(current_log))
+                current_log = ''
+
+            current_log += line
+
+        logEntries.append(LogParser.parse_raw_log(current_log))
+
+        return json.dumps({
+            'messageType': 'logData',
+            'osType': 'Android',
+            'logEntries': logEntries,
+        })
+
+    @staticmethod
+    def parse_raw_log(log_data):
+        parsed_log = re.search(
+            '(.*?) (\d*)-(\d*)\/(.*?) (.)\/(.*?): ((?:.*\\n*)*)', log_data)
+
+        current_year = datetime.now().year
+        log_time = datetime.strptime(
+            '%s-%s' % (str(current_year), parsed_log.group(1)),
+            '%Y-%m-%d %H:%M:%S.%f')
+
+        # Determine the log type
+        log_types = {
+            'I': 'Info',
+            'W': 'Warning',
+            'V': 'Verbose',
+            'E': 'Error',
+            'D': 'Debug',
+            'A': 'WTF',
+        }
+
+        return {
+            'time': log_time,
+            'logType': log_types[parsed_log.group(5)],
+            'tag': parsed_log.group(6),
+            'text': parsed_log.group(7),
+        }
