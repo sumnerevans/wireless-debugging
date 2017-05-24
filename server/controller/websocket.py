77c05af730718eb8cc68a05290159d63528b5f19
Sumner Evans <sumner.evans98@gmail.com>
2017-05-23 20:05:55 -0700

    Basic web app and WebSocket handling (#6)
    * WD-6: starting on some JS WebSockets stuff
    
    * WD-6: added JS libs
    
    * WD-6: fixed bug where disconnecting a WebSocket connection did not actually register with the server
    
    * WD-6: added basic table features
    
    * WD-6: collor to table rows, comments in widb.js
    
    * WD-6: cleaning up widb.js
    
    * refactored widb.js to make websocketOnOpen and websocket.onMessage work
    
    * WD-6: added doc for websocket controller
    
    * compliance with style guides
    
    * WD-6: changed switch statement to an object for coloring table
    
    * WD-6: Updates from code review
    
    * WD-6: added border to table
    
    * WD-6: updates from code review

--- server/controller/websocket.py
+++ server/controller/websocket.py
@@ -3,6 +3,7 @@ WebSocket Controller
 """
 
 import json
+import time
 
 from bottle import route, request, abort
 
@@ -10,38 +11,105 @@ from geventwebsocket import WebSocketError
 
 # Store a dictionary of string -> function
 _ws_routes = {}
+_web_interface_ws_connections = []
+
 
 @route('/ws')
 def handle_websocket():
-    wsock = request.environ.get('wsgi.websocket')
-    if not wsock:
+    """ Handle an incomming WebSocket connection.
+
+    This function handles incomming WebSocket connections and waits for
+    incomming messages from the connection. When a message is recieved, it
+    calls the appropriate function.
+    """
+
+    websocket = request.environ.get('wsgi.websocket')
+    if not websocket:
         abort(400, 'Expected WebSocket request.')
 
-    while True:
+    while not websocket.closed:
         try:
-            message = wsock.receive()
+            message = websocket.receive()
+            if message is None:
+                continue
+
             decoded_message = json.loads(message)
             messageType = decoded_message['messageType']
             if messageType is None:
                 # TODO: blow up
                 pass
 
-            _ws_routes[messageType](decoded_message)
+            _ws_routes[messageType](decoded_message, websocket)
         except WebSocketError:
             break
 
-# Provide a decorator for adding functions to the _ws_route dictionary
+    # Remove the WebSocket connection from the list once it is closed
+    _web_interface_ws_connections.remove(websocket)
+
+
 def ws_router(messageType):
+    """ Provide a decorator for adding functions to the _ws_route dictionary """
     def decorator(function):
         _ws_routes[messageType] = function
 
     return decorator
 
+
 @ws_router('logDump')
-def logDump(message):
-    print('Log Dump: %s' % message)
+def log_dump(message, websocket):
+    """ Handles Log Dumps from the Mobile API
+
+    When a log dump comes in from the Mobile API, this function takes the raw
+    log data, parses it and sends the parsed data to all connected web clients.
+
+    Args:
+        message: the decoded JSON message from the Mobile API
+        websocket: the full websocket connection
+    """
+    # TODO: This function is entirely a placeholder for testing purposes
+    parsed_logs = json.dumps({
+        'messageType': 'logData',
+        'osType': 'Android',
+        'logEntries': [
+            {
+                'time': '2017-11-06T16:34:41.000Z',
+                'text': 'This is not a real error',
+                'tag': 'TEST',
+                'logType': 'Warning',
+            },
+            {
+                'time': '2017-11-06T16:34:50.000Z',
+                'text': 'Cool Log',
+                'tag': 'TEST',
+                'logType': 'Info',
+            },
+            {
+                'time': '2017-11-06T16:34:55.000Z',
+                'text': 'Cool Log',
+                'tag': 'TEST',
+                'logType': 'Error',
+            },
+        ]
+    })
+
+    for connection in _web_interface_ws_connections:
+        connection.send(parsed_logs)
+        time.sleep(1)
+        connection.send(parsed_logs)
 
 
 @ws_router('associateSession')
-def associateSession(message):
-    print('Log Dump: %s' % message)
+def associate_session(message, websocket):
+    """ Associates a WebSocket connection with a session
+
+    When a browser requests to be associated with a session, add the associated
+    WebSocket connection to the list connections for that session.
+
+    Args:
+        message: the decoded JSON message from the Mobile API
+        websocket: the full websocket connection
+    """
+
+    # TODO: Currently we only have one session, when we implement multiple
+    #       connections, modify this to handle it
+    _web_interface_ws_connections.append(websocket)
---
+++ server/resources/js/app/widb.js
@@ -0,0 +1,73 @@
+/*
+ * @fileoverview This contains the main app logic
+ */
+
+/**
+ * Creates a table from log data received through WebSockets
+ * @class
+ */
+class WirelessDebug {
+  /** Initialize the app */
+  constructor() {
+    /** @private @const {!jQuery} */
+    this.logTable_ = $('.log-table');
+
+    /** @private @const {?WebSocket} */
+    this.ws_ = null;
+  }
+
+  /**
+   * Opens a WebSocket connection to the server
+   */
+  start() {
+    this.ws_ = new WebSocket(`ws://${location.host}/ws`);
+    this.ws_.onopen = () => this.websocketOnOpen();
+    this.ws_.onmessage = (message) => this.websocketOnMessage(message);
+  }
+
+  /** Handles WebSocket opening */
+  websocketOnOpen() {
+    let payload = {
+      messageType: 'associateSession',
+    };
+
+    this.ws_.send(JSON.stringify(payload));
+
+    // TODO: get rid of this, only for testing purposes
+    payload = {
+      messageType: 'logDump',
+    };
+
+    this.ws_.send(JSON.stringify(payload));
+  }
+
+  /** Decodes the WebSocket message and adds to table */
+  websocketOnMessage(message) {
+    let messageData = JSON.parse(message.data);
+    if (messageData.messageType === 'logData') {
+      for (let entry of messageData.logEntries) {
+        this.logTable_.append(this.renderLog(entry));
+      }
+    }
+  }
+
+  /** Formats new table entries from log data */
+  renderLog(logEntry) {
+    let color = {
+      'Warning': 'warning',
+      'Error': 'danger',
+    };
+
+    return `<tr class="${color[logEntry.logType]}">
+    <td>${logEntry.time}</td>
+    <td>${logEntry.tag}</td>
+    <td>${logEntry.logType}</td>
+    <td>${logEntry.text}</td>
+    </tr>`;
+  }
+}
+
+/** When the document has been loaded, start Widb */
+$(document).ready(() => {
+  new WirelessDebug().start();
+});
