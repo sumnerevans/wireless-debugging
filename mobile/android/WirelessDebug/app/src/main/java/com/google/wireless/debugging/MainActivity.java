ba251554cdaa6ad96f350e32364a6c06de81f037
ReeceHughes <reecehughes@mymail.mines.edu>
2017-05-25 09:48:27 -0600

    Created Android Studio project and Android test application (#4)
    * Created Android Studio project and Android test application
    
    * Made requested changes from pull request
    
    * Removed auto-gened .gitignore files
    
    * Removed newlines, fixed small style errors

---
+++ mobile/android/WirelessDebug/app/src/main/java/com/google/wireless/debugging/MainActivity.java
@@ -0,0 +1,24 @@
+package com.google.wireless.debugging;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new MainFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
