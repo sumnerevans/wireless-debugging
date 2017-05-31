d76859bf4e30f3ff390ea296044f623c8662b6a0
Daichi Jameson <djameson@mymail.mines.edu>
2017-05-31 16:35:45 -0600

    WD-47, Added user managment interface classes, and a base class for having no authorization. Also modified controller to utilize functions in user management interface. Changed hostname and port to be defined in config.yaml, and obtained using from_config_yaml where it's called

--- server/controller/websocket.py
+++ server/controller/websocket.py
@@ -4,6 +4,7 @@ WebSocket Controller
 
 import json
 import time
+import controller
 
 from bottle import route, request, abort
 from geventwebsocket import WebSocketError
@@ -13,8 +14,9 @@ from helpers import util
 
 
 # Store a dictionary of string -> function
-_ws_routes = {}
-_web_interface_ws_connections = []
+_ws_routes = {} # pylint: disable=invalid-name
+_web_interface_ws_connections = {} # pylint: disable=invalid-name
+_mobile_interface_ws_connections = {} # pylint: disable=invalid-name
 
 
 @route('/ws')
@@ -38,26 +40,38 @@ def handle_websocket():
                 continue
 
             decoded_message = json.loads(message)
-            messageType = decoded_message['messageType']
-            if messageType is None:
+            print(decoded_message['messageType'])
+            message_type = decoded_message['messageType']
+            if message_type is None:
                 # TODO: blow up
                 pass
 
-            _ws_routes[messageType](decoded_message, websocket)
+            _ws_routes[message_type](decoded_message, websocket)
         except WebSocketError:
             break
 
     # Remove the WebSocket connection from the list once it is closed
-    _web_interface_ws_connections.remove(websocket)
+    print(websocket)
+    #_web_interface_ws_connections.remove(websocket)
+    if websocket in _web_interface_ws_connections:
+        print("Websocket's here!")
+        del _web_interface_ws_connections[websocket]
+        print("'Kay, Goodbye!")
 
 
-def ws_router(messageType):
+def ws_router(message_type):
     """ Provide a decorator for adding functions to the _ws_route dictionary """
     def decorator(function):
-        _ws_routes[messageType] = function
+        _ws_routes[message_type] = function
 
     return decorator
 
+@ws_router('startSession')
+def start_session(message, websocket):
+    # TODO: Maybe replace api_key with all of message,
+    # so all of the metadata is retained?
+    _mobile_interface_ws_connections[websocket] = message['apiKey']
+    print(_mobile_interface_ws_connections)
 
 @ws_router('logDump')
 def log_dump(message, websocket):
@@ -70,13 +84,40 @@ def log_dump(message, websocket):
         message: the decoded JSON message from the Mobile API
         websocket: the full websocket connection
     """
-    parsed_logs = LogParser.parse(message)
+    #parsed_logs = LogParser.parse(message)
+    parsed_logs = {
+        "messageType": "logData",
+        "osType": "Android",
+        "logEntries": [{
+            "time": "2017-11-06T16:34:41.000Z",
+            "text": "This is not a real error",
+            "tag": "TEST",
+            "logType": "Warning"
+        }, {
+            "time": "2017-11-06T16:34:41.001Z",
+            "text": "Got here",
+            "tag": "TEST",
+            "logType": "Info"
+        }]
+    }
 
-    for connection in _web_interface_ws_connections:
+    api_key = _mobile_interface_ws_connections[websocket]
+
+    # At first glance this looks like a copy, 
+    # but this is actually grabbing the keys from a dict
+    web_ws_connections = [ws for ws in _web_interface_ws_connections]
+    associated_websockets = ( #
+        controller.user_management_interface.find_associated_websockets(api_key,
+            web_ws_connections))
+    for connection in associated_websockets:
         connection.send(util.serialize_json(parsed_logs))
 
+@ws_router('endSession')
+def end_session(message, websocket):
+    # TODO: Remove websocket from list of websockets
+    del _mobile_interface_ws_connections[websocket]
 
-@ws_router('associateSession')
+@ws_router('associateUser')
 def associate_session(message, websocket):
     """ Associates a WebSocket connection with a session
 
@@ -90,4 +131,11 @@ def associate_session(message, websocket):
 
     # TODO: Currently we only have one session, when we implement multiple
     #       connections, modify this to handle it
-    _web_interface_ws_connections.append(websocket)
+    """ Classic graveyarded code
+    connection = {
+        'api_key': message['apiKey'],
+        'websocket': websocket,
+    }
+    _web_interface_ws_connections.append(connection)
+    """
+    _web_interface_ws_connections[websocket] = message['apiKey']
