ffb4aaa45ae2924db4871b5b8059de593370a946
Sumner Evans <sumner.evans98@gmail.com>
2017-06-01 17:04:33 -0600

    Fixed some bugs with the connection from the Mobile API

--- server/controller/websocket.py
+++ server/controller/websocket.py
@@ -14,8 +14,8 @@ from parsing_lib import LogParser
 from helpers import util
 
 # Store a dictionary of string -> function
-_ws_routes = {} # pylint: disable=invalid-name
-_web_interface_ws_connections = {} # pylint: disable=invalid-name
+_ws_routes = {}  # pylint: disable=invalid-name
+_web_interface_ws_connections = {}  # pylint: disable=invalid-name
 
 
 @route('/ws')
@@ -31,7 +31,7 @@ def handle_websocket():
     if not websocket:
         abort(400, 'Expected WebSocket request.')
 
-    _websocket_metadata = {}
+    websocket_metadata = {}
 
     print('connection received')
 
@@ -47,14 +47,18 @@ def handle_websocket():
                 # TODO: blow up
                 pass
 
-            _ws_routes[message_type](decoded_message, websocket,
-                                     _websocket_metadata)
+            new_metadata = _ws_routes[message_type](decoded_message, websocket,
+                                                    websocket_metadata)
+
+            if new_metadata is not None:
+                websocket_metadata = {**websocket_metadata, **new_metadata}
+
         except WebSocketError:
             break
 
-    #_web_interface_ws_connections.remove(websocket)
     if websocket in _web_interface_ws_connections:
         del _web_interface_ws_connections[websocket]
+
 
 def ws_router(message_type):
     """ Provide a decorator for adding functions to the _ws_route dictionary """
@@ -64,15 +68,21 @@ def ws_router(message_type):
 
     return decorator
 
+
 @ws_router('startSession')
 def start_session(message, websocket, metadata):
     """ Marks the start of a logging session,
         and attaches metadata to the websocket receiving the raw logs
     """
 
-    # There's probably a better way to do this and it should be refactored
-    for attribute, value in message.items():
-        metadata[attribute] = value
+    metadata = {**metadata, **message}
+
+    for key in ('apiKey', 'osType', 'deviceName', 'appName'):
+        if key not in metadata:
+            raise KeyError('startSession message requires %s parameter.' % key)
+
+    return metadata
+
 
 @ws_router('logDump')
 def log_dump(message, websocket, metadata):
@@ -88,26 +98,23 @@ def log_dump(message, websocket, metadata):
 
     parsed_logs = LogParser.parse(message)
 
-    if metadata:
-        api_key = metadata["apiKey"]
-    # This if/else is just to fit with legacy code
-    # TODO: remove this once code is updated on mobile side
-    else:
-        api_key = ""
+    api_key = metadata["apiKey"]
 
-    # At first glance this looks like a copy, 
+    # At first glance this looks like a copy,
     # but this is actually grabbing the keys from a dict
     web_ws_connections = [ws for ws in _web_interface_ws_connections]
-    associated_websockets = ( 
-        controller.user_management_interface.find_associated_websockets(api_key,
-            web_ws_connections))
+    associated_websockets = (
+        controller.user_management_interface.find_associated_websockets(
+            api_key, web_ws_connections))
 
     for connection in associated_websockets:
         connection.send(util.serialize_json(parsed_logs))
 
+
 @ws_router('endSession')
 def end_session(message, websocket, metadata):
     print("currently defunct")
+
 
 @ws_router('associateUser')
 def associate_user(message, websocket, metadata):
