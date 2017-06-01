90292ee80457628f67b6fc07306bb3c26e7bfcc1
Daichi Jameson <djameson@mymail.mines.edu>
2017-06-01 10:47:41 -0600

    Added some pretty trivial tests

---
+++ server/tests/websocket_tests.py
@@ -0,0 +1,14 @@
+"""
+Tests components of the websocket controller
+"""
+
+from controller import websocket
+
+class DummySocket:
+    """ A dummy websocket class.
+
+        Acts as a placeholder for websockets for functions that require
+        websockets. Contains the necessary methods, but provides no actual
+        websocket functionality. 
+    """
+
