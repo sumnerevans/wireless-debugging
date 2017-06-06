1b843e2c42ce0f4b59a46c4345795343f050e60a
Daichi Jameson <djameson@mymail.mines.edu>
2017-06-06 15:36:16 -0600

    Continuing to write tests for email_auth

--- server/tests/email_auth_tests.py
+++ server/tests/email_auth_tests.py
@@ -2,6 +2,8 @@
 Tests the email authorization implementationo of the user management interface.
 """
 
+import os
+
 from user_management_interfaces import email_auth
 from bottle import request, response
 
@@ -14,7 +16,8 @@ def test_get_login():
     umi = email_auth.EmailAuth()
     base_url = 'http://0.0.0.0:80'
 
-    # Ugly format because we can't have any changes in whitespace
+    # Ugly format because we can't have any changes in whitespace so that the 
+    # test works properly.
     target = '''<form action="/login" method="post" accept-charset="UTF-8" class="form-horizontal">
     <div class="form-group">
         <label class="col-sm-2 control-label">Email:</label>
@@ -30,4 +33,37 @@ def test_get_login():
 </form>'''
 
     assert umi.get_login_ui(base_url) == target
+
+
+def test_user_not_logged_in():
+    """ Verify that if the api cookie is not in the user's browswer that the
+        login check returns false.
+    """
+    umi = email_auth.EmailAuth()
+    assert not umi.is_user_logged_in(request)
+
+
+# This test needs to be fixed since it relies on cookies
+def test_user_logged_in():
+    """ Verify that if the api cookie is is in the user's browser that the login
+        check returns true.
+    """
+    umi = email_auth.EmailAuth()
+    request.set_cookie('api_key', 'hello_world')
+
+    assert request.get_cookie('api_key') is not None
+    assert not umi.is_user_logged_in(request)
+
+
+def test_handle_new_login():
+    """ Verify that when a new user logs in they'll be added to the user-api key
+        table, and verify that they successfully log in.
+    """
+    umi = email_auth.EmailAuth()
+    # It'd be nice to actualy make this a temp file
+    umi.user_key_table = "temp_file.txt" 
+
+    
+
+    os.remove(umi.user_key_table)
 
