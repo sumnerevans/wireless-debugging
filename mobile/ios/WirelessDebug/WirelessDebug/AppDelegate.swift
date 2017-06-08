20130a132254e7558ed21b292f84d7a18329bba8
Sumner Evans <sumner.evans98@gmail.com>
2017-06-08 14:27:43 -0600

    made the app not rotate

--- mobile/ios/WirelessDebug/WirelessDebug/AppDelegate.swift
+++ mobile/ios/WirelessDebug/WirelessDebug/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Start the Wireless Debugger
         WirelessDebugger.start(hostname: self.hostname, apiKey: "test")
         return true
     }
